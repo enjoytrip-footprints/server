a8721825047a9b9d75688e9bd225ab455c614ed8
ydol2@naver.com <ydol2@naver.com>
2023-05-19 16:17:32 +0900

    feat: 토큰설정

--- trip/src/main/java/com/ssafy/trip/member/model/repo/MemberRepo.java
+++ trip/src/main/java/com/ssafy/trip/member/model/repo/MemberRepo.java
@@ -2,17 +2,25 @@ package com.ssafy.trip.member.model.repo;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ssafy.trip.member.dto.Member;
+import com.ssafy.trip.member.dto.MemberRegistDto;
 
 @Mapper
 public interface MemberRepo {
 	String loginCheck(Member dto);
 	Member read(String id) throws SQLException;
 	List<Member> readAll() throws SQLException;
-	int create(Member user) throws SQLException;
+	int create(MemberRegistDto user) throws SQLException;
 	int update(Member user) throws SQLException;
 	int delete(String id) throws SQLException;
+	
+	 Member login(Member memberDto) throws SQLException;
+	 Member userInfo(String userid) throws SQLException;
+	 void saveRefreshToken(Map<String, String> map) throws SQLException;
+	 Object getRefreshToken(String userid) throws SQLException;
+	 void deleteRefreshToken(Map<String, String> map) throws SQLException;
 }
